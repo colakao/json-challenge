648e2444c0c97e4bf3f066dadf7a194ccd06d06d
colakao <joaquinleigh@gmail.com>
2021-01-29 16:50:14 -0300

    Project Upload

---
+++ Assets/Scripts/JsonReader.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+public class JsonReader : MonoBehaviour
+{
+    private string _jsonDirectory = Application.streamingAssetsPath.ToString() + "/JsonChallenge.json";
+    private string _json;
+
+    public string title;
+    public List<string> headers;
+    public List<Dictionary<string, string>> data = new List<Dictionary<string, string>>();
+
+
+    public void ReadFile()
+    {
+        _json = File.ReadAllText(_jsonDirectory);
+        JObject _jsonObject = JObject.Parse(_json);
+
+        //Asigna el titulo a title
+        title = _jsonObject["Title"].ToString();
+
+        //Asigna los headers a la lista
+        headers = _jsonObject["ColumnHeaders"].Select(t => (string)t).ToList();
+
+        //Asigna un Dictionary al key/value pair del json y lo mete a la lista
+
+        for (int i = 0; i < _jsonObject["Data"].Children().Count(); i++)
+        {
+            string json = _jsonObject["Data"][i].ToString();
+            data.Add(JsonConvert.DeserializeObject<Dictionary<string, string>>(json));
+        }
+    }
+
+    //Limpia las variables que cambian
+    public void ClearValues()
+    {
+        title = "";
+        headers.Clear();
+        data.Clear();
+    }
+}
+
+
---
+++ Assets/Scripts/UIDisplay.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+//Es común en mi código el spanglish
+public class UIDisplay : MonoBehaviour
+{
+    public GameObject rowObject;
+    public Text textObject;
+    public Transform rowsTransform;
+    public JsonReader json;
+
+    public void ReloadUI()
+    {
+        if (json.data != null || json.headers != null || json.title != "")
+        json.ClearValues();
+
+        //Load/Reload File
+        json.ReadFile();
+        if (rowsTransform.childCount > 0)
+        {
+            foreach (Transform child in rowsTransform)
+            {
+                //No es muy eficiente pero cumple su función
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+
+        //Load title
+        GameObject firstRow = Instantiate(rowObject, rowsTransform);
+        Text title = Instantiate(textObject, firstRow.transform);
+
+        title.text = json.title.ToString();
+        title.fontStyle = FontStyle.Bold;
+
+        //Load rowObject and headers as text
+        GameObject secondRow = Instantiate(rowObject, rowsTransform);
+        for (int i = 0; i < json.headers.Count; i++)
+        {
+            Text localText = Instantiate(textObject, secondRow.transform);
+
+            localText.text = json.headers[i].ToString();
+            localText.fontStyle = FontStyle.Bold;
+        }
+
+        //Load more rowObjects and data as text
+        for (int i = 0; i < json.data.Count; i++)
+        {
+            GameObject dataRow = Instantiate(rowObject, rowsTransform);
+            foreach (var value in json.data[i].Values)
+            {
+                Text localText = Instantiate(textObject, dataRow.transform);
+
+                localText.text = value.ToString();
+            }
+        }
+    }
+}
